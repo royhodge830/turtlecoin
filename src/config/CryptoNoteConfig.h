7b42b9211fe46c27e3c0ba420a538e6fd8eabec2
Roy Hodge (SMTX) <royhodge830@gmail.com>
2019-10-28 12:58:00 -0700

    Update CryptoNoteConfig.h

--- src/config/CryptoNoteConfig.h
+++ src/config/CryptoNoteConfig.h
@@ -272,7 +272,7 @@ namespace CryptoNote
         const char MINER_CONFIG_FILE_NAME[] = "miner_conf.json";
     } // namespace parameters
 
-    const char CRYPTONOTE_NAME[] = "TurtleCoin";
+    const char CRYPTONOTE_NAME[] = "UniBit";
 
     const uint8_t TRANSACTION_VERSION_1 = 1;
 
@@ -353,16 +353,9 @@ namespace CryptoNote
     const std::string LICENSE_URL = "https://github.com/turtlecoin/turtlecoin/blob/master/LICENSE";
 
     const static boost::uuids::uuid CRYPTONOTE_NETWORK = {
-        {0xb5, 0x0c, 0x4a, 0x6c, 0xcf, 0x52, 0x57, 0x41, 0x65, 0xf9, 0x91, 0xa4, 0xb6, 0xc1, 0x43, 0xe9}};
+        {0x66, 0x00, 0x44, 0x5c, 0xcf, 0x52, 0x57, 0x41, 0x65, 0xf9, 0x09, 0xa4, 0xb6, 0xc1, 0x43, 0x66}};
 
     const char *const SEED_NODES[] = {
-        "206.189.142.142:11897", // rock
-        "145.239.88.119:11999", // cision
-        "142.44.242.106:11897", // tom
-        "165.227.252.132:11897", // iburnmycd
-        "148.251.178.238:11897", // hv
-        "45.32.138.7:11897", // extra
-        "46.214.70.196:11897", // CuveeRO
-        "91.239.237.54:11897" // CuveeCZ
+        "206.189.142.142:11897", // primary vps
     };
 } // namespace CryptoNote
